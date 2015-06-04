bf41dfc6f3220ef9d3e62b57e88a07309219fec9
Hiroshi Yoshida <hrysd22@gmail.com>
2015-06-04 15:47:41 +0900

    Initialize commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# idobata-electron
+
+This is unofficial [Idobata](https://idobata.io) desktop client.
+
+## How to launch applicatoin
+
+First, clone this repository then run following commands.
+
+```
+$ npm install
+$ npm run idobata
+```
---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const app = require('app');
+const BrowserWindow = require('browser-window');
+
+require('crash-reporter').start();
+
+app.on('window-all-closed', function() {
+  app.quit();
+})
+
+app.on('ready', function() {
+  let mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  mainWindow.loadUrl(`file://${__dirname}/index.html`);
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  })
+})
