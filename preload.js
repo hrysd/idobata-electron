92a4bd2ba41c2bde1fb4f35cdbceb2ec34a7d50a
aikyo <aikyo02@gmail.com>
2015-07-13 20:50:45 +0900

    Remove unnecessary code.

--- preload.js
+++ preload.js
@@ -1,34 +1,21 @@
 (function(){  
-  var onMessageCreated = function(user, store, router) {
+  var onMessageCreated = function(user, store) {
     return function(data){
-      var notify = false;
-      var mode = 'all'; 
-      if (mode == 'all') {
-        notify = true;
-      } else if (mode == 'mention') {
-        if (data.message.mentions.indexOf(parseInt(user.get('id'))) >= 0) {
-          notify = true;
-        }
-      }
-      if (notify) {
-        store.find('message', data.message.id).then(function(message) {
-          var title = data.message.senderName;
-          new Notification(title, {
-            body: data.message.bodyPlain,
-            icon: data.message.senderIconUrl
-          })
-        });
-      }
+      store.find('message', data.message.id).then(function(message) {
+        var title = data.message.senderName;
+        new Notification(title, {
+          body: data.message.bodyPlain,
+        icon: data.message.senderIconUrl
+        })
+      });
     }
   };
   window.addEventListener('ready.idobata', function(e) {
     var container = e.detail.container;
-    
     var pusher = container.lookup('pusher:main');
-    var user   = container.lookup('service:session').get('user');
+    var user   = container.lookup('service:session').get('user.id');
     var store  = container.lookup('store:main');
-    var router = container.lookup('router:main');
     
-    pusher.bind('message:created', onMessageCreated(user, store, router));
+    pusher.bind('message:created', onMessageCreated(user, store));
   });
 })();
