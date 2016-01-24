7d23fca9ab62ebc2d31f2d5bc7b4f0243d8a8b68
Hiroshi Yoshida <hrysd22@gmail.com>
2016-01-24 18:50:20 +0900

    Fix notification

--- preload.js
+++ preload.js
@@ -2,10 +2,11 @@
   var onMessageCreated = function(user, store) {
     return function(data) {
       store.find('message', data.message.id).then(function(message) {
-        var title = data.message.senderName;
+        var title = message.get('senderName');
+
         new Notification(title, {
-          body: data.message.bodyPlain,
-          icon: data.message.senderIconUrl
+          body: message.get('bodyPlain'),
+          icon: message.get('senderIconUrl')
         });
       });
     }
@@ -15,7 +16,7 @@
     var container = e.detail.container;
     var pusher = container.lookup('pusher:main');
     var user   = container.lookup('service:session').get('user.id');
-    var store  = container.lookup('store:main');
+    var store  = container.lookup('service:store');
 
     pusher.bind('message:created', onMessageCreated(user, store));
   });
